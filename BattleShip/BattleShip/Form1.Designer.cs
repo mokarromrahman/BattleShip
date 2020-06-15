27c48acc04761352ad1e0607fdd699434a602c72
Mokarrom Rahman <mrahman0000@gmail.com>
2020-06-15 17:28:53 -0600

    Tested panel and started to create UI.

--- BattleShip/BattleShip/Form1.Designer.cs
+++ BattleShip/BattleShip/Form1.Designer.cs
@@ -28,13 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this._drawingPanel = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this._btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _drawingPanel
+            // 
+            this._drawingPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this._drawingPanel.Location = new System.Drawing.Point(25, 28);
+            this._drawingPanel.Name = "_drawingPanel";
+            this._drawingPanel.Size = new System.Drawing.Size(564, 479);
+            this._drawingPanel.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(607, 513);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 61);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(606, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(193, 479);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 513);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 516);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Add Friend:";
+            // 
+            // _btnSend
+            // 
+            this._btnSend.Location = new System.Drawing.Point(759, 550);
+            this._btnSend.Name = "_btnSend";
+            this._btnSend.Size = new System.Drawing.Size(40, 23);
+            this._btnSend.TabIndex = 5;
+            this._btnSend.Text = "Send";
+            this._btnSend.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(806, 585);
+            this.Controls.Add(this._btnSend);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this._drawingPanel);
+            this.Name = "Form1";
+            this.Text = "Battle Ship";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel _drawingPanel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button _btnSend;
     }
 }
 
--- BattleShip/BattleShip/Form1.cs
+++ BattleShip/BattleShip/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace BattleShip
 {
@@ -15,6 +16,21 @@ namespace BattleShip
         public Form1()
         {
             InitializeComponent();
+
+            _drawingPanel.Paint += _drawingPanel_Paint;
+            Click += Form1_Click;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            _drawingPanel.Visible = !_drawingPanel.Visible;
+        }
+
+        private void _drawingPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = CreateGraphics();
+
+            e.Graphics.DrawRectangle(new Pen(new SolidBrush(Color.Red), 1), new Rectangle(25, 25, 25, 30));
         }
     }
 }
